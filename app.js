819e52dac3ea9b34e9c9fef7f756a1d35d527cc9
AmirHChabiZadeh <amirh.chabi84zadeh@gmail.com>
2023-05-22 21:02:59 +0430

    HEART ACE
    اضافه کردن کادر متن به صفحه

---
+++ app.js
@@ -0,0 +1,40 @@
+function yellow(){
+    let x, y;
+    x =
+    document.getElementById("box");
+    y =
+    document.createElement("textarea");
+    y.style ="background-color: #E4EE90";
+    x.appendChild(y)
+}
+
+function blue(){
+    let x, y;
+    x =
+    document.getElementById("text");
+    y =
+    document.createElement("textarea");
+    y.style ="background-color: #01D3FF";
+    x.appendChild(y)
+}
+
+
+function red(){
+    let x, y;
+    x =
+    document.getElementById("text");
+    y =
+    document.createElement("textarea");
+    y.style ="background-color: #FF9B73";
+    x.appendChild(y)
+}
+
+function orange(){
+    let x, y;
+    x =
+    document.getElementById("text");
+    y =
+    document.createElement("textarea");
+    y.style ="background-color:  #FFC972";
+    x.appendChild(y)
+}
