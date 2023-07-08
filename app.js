812b2a02463ce238b12cb3a4acd6076fc88842d7
fari-panisher <farhad.13ahmadi.83@gmail.com>
2023-07-08 23:22:26 +0430

    sticky note
    1-add note to list=done
    2-add note to local storage=done
    3-find note(select with JS)=problem

--- app.js
+++ app.js
@@ -1,138 +1,77 @@
-// let test=document.getElementById('boxs').style.display='none'
-let x, y, w, h, b;
+let noteDiv, textarea, newDiv, input, button;
 
-function yellow() {
+function newNote(colorName) {
     // find element and called x
-    x =
+    noteDiv =
         document.getElementById("notes");
     // create div and called w
-    w =
-        document.createElement("div");
-    w.style = "background-color:#E4EE90 "
-    // create input and called h
-    h = document.createElement("input");
-    h.type = "text"
-    h.placeholder = 'title name'
-    h.style = "background-color:#E4EE90 "
-    // create textarea and called y
-    y =
-        document.createElement("textarea");
-    y.placeholder = "type here";
-    y.style = "background-color:#E4EE90 "
-    // create button for delete and called b
-    b = document.createElement('button')
-    b.type = "button"
-    b.className='delete'
-    // add b to w
-    w.appendChild(b)
-    // add h to w
-    w.appendChild(h)
-    // add y to w
-    w.appendChild(y)
+    // newDiv
+    //     =
+    //     document.createElement("div");
+    // newDiv.style = "background-color:#E4EE90 "
+    // // create input and called h
+    // input = document.createElement("input");
+    // input.type = "text"
+    // input.placeholder = 'title name'
+    // input.style = "background-color:#E4EE90 "
+    // // create textarea and called y
+    // textarea =
+    //     document.createElement("textarea");
+    // textarea.placeholder = "type here";
+    // textarea.style = "background-color:#E4EE90 "
+    // // create button for delete and called b
+    // button = document.createElement('button')
+    // button.type = "button"
+    // button.className = 'delete'
+    // // add b to w
+    // newDiv.appendChild(button)
+    // // add h to w
+    // newDiv.appendChild(input)
+    // // add y to w
+    // newDiv.appendChild(textarea)
+
+
+    newDiv.innerHTML += ` <div>
+
+    <input type="text" placeholder="title name">
+    <textarea name="" placeholder="type here..."></textarea>
+
+
+    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M19 6.90152L12.1695 6.31415L9.5 0.042572L6.8305 6.32363L0 6.90152L5.187 11.3826L3.629 18.0426L9.5 14.5089L15.371 18.0426L13.8225 11.3826L19 6.90152ZM9.5 12.7373L5.928 14.8878L6.878 10.8331L3.724 8.10468L7.885 7.74468L9.5 3.92678L11.1245 7.75415L15.2855 8.11415L12.1315 10.8426L13.0815 14.8973L9.5 12.7373Z"
+            fill="#C9D940" />
+    </svg>
+    </svg>
+    <svg class="pencil" fill="#414141" viewBox="-2.4 -2.4 28.80 28.80"
+        xmlns="http://www.w3.org/2000/svg">
+        <g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(4.92,4.92), scale(0.59)">
+            <rect x="-2.4" y="-2.4" width="28.80" height="28.80" rx="14.4" fill="#ECECEC"
+                strokewidth="0"></rect>
+        </g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC"
+            stroke-width="0.048"></g>
+        <g id="SVGRepo_iconCarrier">
+            <path d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2ZM10,16,7,17l1-3,7-7,2,2Z"></path>
+        </g>
+    </svg>
+
+</div>`
     // add w to x
-    x.appendChild(w)
-    return x
+    noteDiv.appendChild(newDiv)
+
+    newDiv.classList.add(colorName)
+    // console.log(noteDiv);
+    // return noteDiv
+    console.log(newDiv);
 
 }
 
-function blue() {
-    // find element and called x
-    x =
-        document.getElementById("notes");
-    // create div and called w
-    w =
-        document.createElement("div");
-    w.style = "background-color:#01D3FF "
-    // create input and called h
-    h = document.createElement("input");
-    h.placeholder = 'title name'
-    h.style = "background-color:#01D3FF "
-    // create textarea and called y
-    y =
-        document.createElement("textarea");
-    y.placeholder = "type here";
-    y.style = "background-color:#01D3FF "
-    // create button for delete and called b
-    b = document.createElement('button')
-    b.type = "button"
-    b.className='delete'
-    // add b to w
-    w.appendChild(b)
-    // add h to w
-    w.appendChild(h)
-    // add y to w
-    w.appendChild(y)
-    // add w to x
-    x.appendChild(w)
-return x
 
-}
 
-function red() {
 
-    // find element and called x
-    x =
-        document.getElementById("notes");
-    // create div and called w
-    w =
-        document.createElement("div");
-    w.style = "background-color:#FF9B73 "
-    // create input and called h
-    h = document.createElement("input");
-    h.placeholder = 'title name'
-    h.style = "background-color:#FF9B73 "
-    // create textarea and called y
-    y =
-        document.createElement("textarea");
-    y.placeholder = "type here";
-    y.style = "background-color:#FF9B73 "
-    // create button for delete and called b
-    b = document.createElement('button')
-    b.type = "button"
-    b.className='delete'
-    // add b to w
-    w.appendChild(b)
-    // add h to w
-    w.appendChild(h)
-    // add y to w
-    w.appendChild(y)
-    // add w to x
-    x.appendChild(w)
-return x
-}
 
-function orange() {
 
-    // find element and called x
-    x =
-        document.getElementById("notes");
-    // create div and called w
-    w =
-        document.createElement("div");
-    w.style = "background-color:#FFC972 "
-    // create input and called h
-    h = document.createElement("input");
-    h.placeholder = 'title name'
-    h.style = "background-color:#FFC972 "
-    // create textarea and called y
-    y =
-        document.createElement("textarea");
-    y.placeholder = "type here";
-    y.style = "background-color:#FFC972 "
-    // create button for delete and called b
-    b = document.createElement('button')
-    b.type = "button"
-     b.className='delete'
-    // add b to w
-    w.appendChild(b)
-    // add h to w
-    w.appendChild(h)
-    // add y to w
-    w.appendChild(y)
-    // add w to x
-    x.appendChild(w)
-    return x
-}
 
 
 
--- sicky note/note.js
+++ sicky note/note.js
@@ -1,71 +1,187 @@
+// let container2 = document.getElementsByClassName("container2")[0];
+// let container3 = document.getElementsByClassName("container3")[0];
+// let checkIcon = document.getElementById("check-icon");
+// let xIcon = document.getElementById("x-icon");
+// let i =0;
+
+// xIcon.addEventListener("click",function(){
+//     typeNote();
+// })
+
+// checkIcon.addEventListener("click",function(){
+//     createNote();
+// })
+
+// function typeNote(){
+//     if (container3.style.display == "none") {
+//         container3.style.display = "block";
+//     }
+//     else{
+//         container3.style.display = "none";
+//     }
+// }
+
+// function createNote(){
+//     let noteText = document.getElementById("note-text").value;
+//     let node0 = document.createElement("div");
+//     let node1 = document.createElement("h1");
+
+//     node1.innerHTML = noteText;
+//     node1.setAttribute("style","width:250px;height:250px;font-size:26px;padding:25px;margin-top:10px;overflow:hidden;box-shadow: 0px 10px 24px 0px rgba(0,0,0,0.75)")
+
+//     node1.style.margin = margin();
+//     node1.style.transform = rotate();
+//     node1.style.background = color();
+
+//     node0.appendChild(node1);
+
+//     container2.insertAdjacentElement("beforeend",node0);
+
+//     node0.addEventListener("mouseenter",function(){
+//         node0.style.transform = "scale(1.1)";
+//     })
+
+//     node0.addEventListener("mouseleave",function(){
+//         node0.style.transform = "scale(1)"
+//     })
+
+//     node0.addEventListener("dblclick",function(){
+//         node0.remove();
+//     })
+
+//     document.getElementById("note-text").value = "";
+// }
+
+// function margin(){
+//     let randomMargin = ["-5px","1px","5px","10px","15px","20px"]
+//     return randomMargin[Math.ceil(Math.random()*randomMargin.length)]
+// }
+
+// function rotate(){
+//     let randomRotate = ["rotate(3deg)","rotate(1deg)","rotate(-1deg)","rotate(-3deg)","rotate(-5deg)","rotate(-10deg)"]
+//     return randomRotate[Math.floor(Math.random()*randomRotate.length)]
+// }
+
+// function color(){
+//     let randomColor = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328",]
+//     if(i > randomColor.length - 1){
+//         i = 0
+//     }
+//     return randomColor[i++]
+// }
+
+
+
+// our oen way:)...................................
+
+// اضافه کردن نوت به لیست نوت ها===done
+// ذخیره نوت در حافظه===done
+// حذف نوت از حافظه
+// حذف نوت از لیست نوت ها
+// نشان دادن تمامی نوت ها در زمان لود صفحه
+
+let container1 = document.getElementsByClassName("container1")[0];
 let container2 = document.getElementsByClassName("container2")[0];
 let container3 = document.getElementsByClassName("container3")[0];
+let formNote = document.getElementById("formNote")
 let checkIcon = document.getElementById("check-icon");
-let xIcon = document.getElementById("x-icon");
-let i =0;
+let removeNote = document.querySelector('x-icon')
+let noteText = document.getElementById('note-text')
+let note = document.getElementsByClassName('note')
+let noteList = document.getElementById('noteList')
+// برای نمایش فرم نوت برای اضافه کردن نوت 
+container1.addEventListener('click', formOfNote)
 
-xIcon.addEventListener("click",function(){
-    typeNote();
-})
+// showing form Note
+function formOfNote() {
+    container3.style.display = 'block'
+}
+// for add new note in page and LS
+checkIcon.addEventListener('click', addNewNote)
+// for remove note in list & LS
+// noteList.addEventListener('click ', remove)
 
-checkIcon.addEventListener("click",function(){
-    createNote();
-})
-
-function typeNote(){
-    if (container3.style.display == "none") {
-        container3.style.display = "block";
-    }
-    else{
-        container3.style.display = "none";
-    }
+// template of not
+function template(note, noteId) {
+    return `
+    <li data-id='${noteId}' class="note">
+        <span>${note}</span>
+        <a class='removeBtn' href="#">❌</a>
+    </li>`
 }
 
-function createNote(){
-    let noteText = document.getElementById("note-text").value;
-    let node0 = document.createElement("div");
-    let node1 = document.createElement("h1");
+function addNoteInList(noteText, noteId) {
+    container2.
+    insertAdjacentHTML
+        ('beforebegin', template(noteText, noteId))
+}
 
-    node1.innerHTML = noteText;
-    node1.setAttribute("style","width:250px;height:250px;font-size:26px;padding:25px;margin-top:10px;overflow:hidden;box-shadow: 0px 10px 24px 0px rgba(0,0,0,0.75)")
+function addNewNote(e) {
+    e.preventDefault()
+    let newNote = noteText.value
+    let noteId = (Math.random() * 100000).toFixed()
+    // add in list:
+    addNoteInList(newNote, noteId)
+    // add is LS:
+    addNoteInLS(newNote, noteId)
+    // value of trxtarea:
+    formNote.reset()
+    // hidde div:
+    container3.style.display = "none"
+}
 
-    node1.style.margin = margin();
-    node1.style.transform = rotate();
-    node1.style.background = color();
-
-    node0.appendChild(node1);
-    
-    container2.insertAdjacentElement("beforeend",node0);
-    
-    node0.addEventListener("mouseenter",function(){
-        node0.style.transform = "scale(1.1)";
+function addNoteInLS(noteText, noteId) {
+    // load in LS
+    let noteLS = loadOfLS()
+    // push notes in LS
+    noteLS.push({
+        text: noteText,
+        ID: noteId
     })
-    
-    node0.addEventListener("mouseleave",function(){
-        node0.style.transform = "scale(1)"
+    // save changes in LS
+    saveNoteInLS(noteLS)
+
+}
+let click=addEventListener('click',function (e) {
+    console.log(e.target.classList.contains('removeBtn'));
+    })
+    noteList.addEventListener('click ', function(event) {
+        if (event.target.classList.contains('removeBtn')) {
+            console.log(event.target);
+        }
     })
 
-    node0.addEventListener("dblclick",function(){
-        node0.remove();
-    })
-    
-    document.getElementById("note-text").value = "";
-}
 
-function margin(){
-    let randomMargin = ["-5px","1px","5px","10px","15px","20px"]
-    return randomMargin[Math.ceil(Math.random()*randomMargin.length)]
-}
+// remove function
+// function remove(event) {
+//     if (event.target.classList.contains('removeBtn')) {
+//         event.target.parentElement.remove()
+//     }
+    // 1-remove from list
 
-function rotate(){
-    let randomRotate = ["rotate(3deg)","rotate(1deg)","rotate(-1deg)","rotate(-3deg)","rotate(-5deg)","rotate(-10deg)"]
-    return randomRotate[Math.floor(Math.random()*randomRotate.length)]
-}
+// }
 
-function color(){
-    let randomColor = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328",]
-    if(i > randomColor.length - 1){
-        i = 0
+
+function loadOfLS() {
+    let noteLS = JSON.parse(localStorage.getItem('Note'))
+
+    if (noteLS == null) {
+        localStorage.setItem('Note', '[]')
+        let noteLS = JSON.parse(localStorage.getItem('Notes'))
     }
-    return randomColor[i++]
+    return noteLS
 }
+
+function saveNoteInLS(note) {
+    let noteLS = note
+    noteLS = JSON.stringify(noteLS)
+    localStorage.setItem('Note', noteLS)
+}
+// style:
+// note.addEventListener("mouseenter", function () {
+//     note.style.transform = "scale(2)";
+// })
+
+// note.addEventListener("mouseleave", function () {
+//     note.style.transform = "scale(1)"
+// })
