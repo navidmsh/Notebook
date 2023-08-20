55c61f57e891df573eba900efecf8cb532612065
fari-panisher <farhad.13ahmadi.83@gmail.com>
2023-08-20 16:34:41 +0430

    note

--- sicky note/note.js
+++ sicky note/note.js
@@ -1,76 +1,3 @@
-// let container2 = document.getElementsByClassName("container2")[0];
-// let container3 = document.getElementsByClassName("container3")[0];
-// let checkIcon = document.getElementById("check-icon");
-// let xIcon = document.getElementById("x-icon");
-// let i =0;
-
-// xIcon.addEventListener("click",function(){
-//     typeNote();
-// })
-
-// checkIcon.addEventListener("click",function(){
-//     createNote();
-// })
-
-// function typeNote(){
-//     if (container3.style.display == "none") {
-//         container3.style.display = "block";
-//     }
-//     else{
-//         container3.style.display = "none";
-//     }
-// }
-
-// function createNote(){
-//     let noteText = document.getElementById("note-text").value;
-//     let node0 = document.createElement("div");
-//     let node1 = document.createElement("h1");
-
-//     node1.innerHTML = noteText;
-//     node1.setAttribute("style","width:250px;height:250px;font-size:26px;padding:25px;margin-top:10px;overflow:hidden;box-shadow: 0px 10px 24px 0px rgba(0,0,0,0.75)")
-
-//     node1.style.margin = margin();
-//     node1.style.transform = rotate();
-//     node1.style.background = color();
-
-//     node0.appendChild(node1);
-
-//     container2.insertAdjacentElement("beforeend",node0);
-
-//     node0.addEventListener("mouseenter",function(){
-//         node0.style.transform = "scale(1.1)";
-//     })
-
-//     node0.addEventListener("mouseleave",function(){
-//         node0.style.transform = "scale(1)"
-//     })
-
-//     node0.addEventListener("dblclick",function(){
-//         node0.remove();
-//     })
-
-//     document.getElementById("note-text").value = "";
-// }
-
-// function margin(){
-//     let randomMargin = ["-5px","1px","5px","10px","15px","20px"]
-//     return randomMargin[Math.ceil(Math.random()*randomMargin.length)]
-// }
-
-// function rotate(){
-//     let randomRotate = ["rotate(3deg)","rotate(1deg)","rotate(-1deg)","rotate(-3deg)","rotate(-5deg)","rotate(-10deg)"]
-//     return randomRotate[Math.floor(Math.random()*randomRotate.length)]
-// }
-
-// function color(){
-//     let randomColor = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328",]
-//     if(i > randomColor.length - 1){
-//         i = 0
-//     }
-//     return randomColor[i++]
-// }
-
-
 
 // our oen way:)...................................
 
@@ -85,10 +12,11 @@ let container2 = document.getElementsByClassName("container2")[0];
 let container3 = document.getElementsByClassName("container3")[0];
 let formNote = document.getElementById("formNote")
 let checkIcon = document.getElementById("check-icon");
-let removeNote = document.querySelector('x-icon')
+let removeNote = document.querySelector('#x-icon')
 let noteText = document.getElementById('note-text')
 let note = document.getElementsByClassName('note')
 let noteList = document.getElementById('noteList')
+let noteTitle = document.querySelector('.noteTitle')
 // برای نمایش فرم نوت برای اضافه کردن نوت 
 container1.addEventListener('click', formOfNote)
 
@@ -98,43 +26,61 @@ function formOfNote() {
 }
 // for add new note in page and LS
 checkIcon.addEventListener('click', addNewNote)
-// for remove note in list & LS
-// noteList.addEventListener('click ', remove)
+
+removeNote.addEventListener('click', () => {
+    container3.style.display = 'none'
+})
+
+
 
 // template of not
-function template(note, noteId) {
+function template(title, note, noteId) {
     return `
     <li data-id='${noteId}' class="note">
+        <h3>${title}</h3>
+        <button class='removeBtn'>
+  <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>
+        </button>
         <span>${note}</span>
-        <a class='removeBtn' href="#">❌</a>
+        
     </li>`
 }
 
-function addNoteInList(noteText, noteId) {
-    container2.
+function addNoteInList(noteTitle, noteText, noteId) {
+    noteList.
     insertAdjacentHTML
-        ('beforebegin', template(noteText, noteId))
+        ('afterbegin', template(noteTitle, noteText, noteId))
+        let noteLi= document.querySelector('#noteList li')
+
+        // margin for notes
+        noteLi.style.margin=margin()
+        // return for note
+        noteLi.style.rotate=rotate()
+        // colots for notes
+        noteLi.style.backgroundColor=color()
 }
 
 function addNewNote(e) {
     e.preventDefault()
+    let title = noteTitle.value
     let newNote = noteText.value
     let noteId = (Math.random() * 100000).toFixed()
     // add in list:
-    addNoteInList(newNote, noteId)
+    addNoteInList(title, newNote, noteId)
     // add is LS:
-    addNoteInLS(newNote, noteId)
+    addNoteInLS(title, newNote, noteId)
     // value of trxtarea:
     formNote.reset()
     // hidde div:
     container3.style.display = "none"
 }
 
-function addNoteInLS(noteText, noteId) {
+function addNoteInLS(title, noteText, noteId) {
     // load in LS
     let noteLS = loadOfLS()
     // push notes in LS
     noteLS.push({
+        title: title,
         text: noteText,
         ID: noteId
     })
@@ -142,25 +88,36 @@ function addNoteInLS(noteText, noteId) {
     saveNoteInLS(noteLS)
 
 }
-let click=addEventListener('click',function (e) {
-    console.log(e.target.classList.contains('removeBtn'));
-    })
-    noteList.addEventListener('click ', function(event) {
-        if (event.target.classList.contains('removeBtn')) {
-            console.log(event.target);
-        }
-    })
 
 
-// remove function
-// function remove(event) {
-//     if (event.target.classList.contains('removeBtn')) {
-//         event.target.parentElement.remove()
-//     }
-    // 1-remove from list
+noteList.addEventListener('click', remove)
 
-// }
+function remove(event) {
+    // remove from list
+    if (event.target.classList.contains('removeBtn')) {
+        console.log(event);
+        event.target.parentElement.remove()
+    }
+    // remove from LS
+    let noteId = event.target.parentElement.getAttribute('data-id')
+    removeFromLS(noteId)
+}
 
+function removeFromLS(Id) {
+    let LsNote = loadOfLS()
+
+    LsNote.forEach(
+        function (eachNote, noteId) {
+            if (Id == eachNote.ID) {
+                LsNote.splice(noteId, 1)
+            }
+        });
+
+    saveNoteInLS(LsNote);
+}
+
+
+container1.addEventListener('click', loadOfLS)
 
 function loadOfLS() {
     let noteLS = JSON.parse(localStorage.getItem('Note'))
@@ -177,11 +134,25 @@ function saveNoteInLS(note) {
     noteLS = JSON.stringify(noteLS)
     localStorage.setItem('Note', noteLS)
 }
-// style:
-// note.addEventListener("mouseenter", function () {
-//     note.style.transform = "scale(2)";
-// })
+// margin for note
+function margin(){
+    let randomMargin = ["-5px","1px","5px","10px","15px","20px"]
+    let random=Math.floor(Math.random() * randomMargin.length)
+    console.log(randomMargin[random]);
+    return randomMargin[random]
+}
+// rotation for note
+function rotate(){
+    let randomRotate = ["3deg","1deg","-1deg","-3deg","-5deg","-10deg"]
+    let random=Math.floor(Math.random() * randomRotate.length)
+    console.log(randomRotate[random]);
+    return randomRotate[random]
+}
+// color for note
 
-// note.addEventListener("mouseleave", function () {
-//     note.style.transform = "scale(1)"
-// })
+function color(){
+    let i = 7
+    let randomColor = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328",]
+ let random=Math.floor(Math.random()*randomColor.length)
+ return randomColor[random]
+}
